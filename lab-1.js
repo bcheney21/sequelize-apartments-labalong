5381ecd5ad8fec3845b8787f86bc5aef6b5a5e13
weston-bailey <wdbailey87@gmail.com>
2021-02-18 11:19:56 -0800

    remove solutions

--- lab-1.js
+++ lab-1.js
@@ -3,137 +3,22 @@
  */
 
 // 1. require your models
-const db = require('./models')
 
 // 2. CREATE an owner John age 33 and console log the result
 // https://sequelize.org/master/manual/model-querying-basics.html#simple-insert-queries
-async function createOwner() {
-  try {
-    // create returns the row that was created
-    const createdOwner = await db.owner.create({
-      name: 'John',
-      age: 33
-    })
-    console.log(`created new owner: ${createdOwner.name} who is ${createdOwner.age}`)
-  } catch (err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see output
-createOwner()
-
 
 // 3. CREATE a property named Brookevista with 20 units
 // https://sequelize.org/master/manual/model-querying-basics.html#simple-insert-queries
-async function createProperty() {
-  try {
-    const newProperty = await db.property.create({
-      name: 'Brookvista',
-      units: 20
-    })
-
-    console.log(`created new property ${newProperty.name}`)
-  } catch (err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see output
-// createProperty()
 
 // 4. READ all owners and console log thier names
 // https://sequelize.org/master/manual/model-querying-basics.html#simple-select-queries
-async function ownerNames() {
-  try {
-    // findAll returns an iterable
-    const owners = await db.owner.findAll()
-  
-    // log the names of each owner
-    owners.forEach(owner => {
-      console.log(owner.name)
-    })
-  } catch (err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see output
-// ownerNames()
 
 // 5. READ the property with an id of 1 and log the name and id
 // https://sequelize.org/master/manual/model-querying-finders.html
-// solve with findByPk
-async function solveOne() {
-  try {
-    const property = await db.property.findByPk(1)
-    console.log(property.name, property.id)
-  } catch(err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see output
-// solveOne()
-
-// solve with findOne and a where clause
-async function solveTwo() {
-  try {
-    const options = {
-      where: {
-        id: 1
-      }
-    }
-    const property = await db.property.findOne(options)
-    console.log(property.name, property.id)
-  } catch(err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see output
-// solveTwo()
 
 // 6. UPDATE the owner named John's age to 43 console log the rows updated
 // https://sequelize.org/master/manual/model-querying-basics.html#simple-select-queries
-async function updateOwner() {
-  try {
-    // what to update
-    const update = {
-      age: 43
-    }
-    // where to updated
-    const options = {
-      where: {
-        name: 'John'
-      }
-    }
-    // takes the columns to update as first argument
-    // update returns the number of rows that where updated
-    const rowsUpdated = await db.owner.update(update, options)
-    console.log(`number of rows changed: ${rowsUpdated}`)
-  } catch(err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see output
-// updateOwner()
 
 // 7. DESTROY the owner named John 
 // https://sequelize.org/master/manual/model-querying-basics.html#simple-delete-queries
-async function destroyOwner() {
-  try {
-    // returns the number of rows deleted from the db
-    const numRowsDeleted = await db.owner.destroy({
-      where: { 
-        name: 'John' 
-      }
-    })
-    console.log(`${numRowsDeleted} removed from the db`)
-  } catch (error) {
-    console.log(error)
-  }
-}
 
-// destroyOwner()
--- lab-2.js
+++ lab-2.js
@@ -3,103 +3,21 @@
  */
 
 // 1. require your models
-const db = require('./models')
 
 // 2. READ an owner with thier id and include thier properties, then console log the name of all of thier properties
 // you can check in your psql shell to find an owner with properties!
 // https://sequelize.org/master/manual/model-querying-finders.html
 // https://sequelize.org/master/manual/eager-loading.html
-async function findProperties() {
-  try {
-    const owner = await db.owner.findByPk(2, {
-      include: [db.property]
-    }) 
-    owner.properties.forEach(property => {
-      console.log(property.name)
-    })
-  } catch (err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
 
 // 3. READ one of the owners from the db (your choice) and also the property named Brookevista. Add the Brookevista to the owner.
 // https://sequelize.org/master/manual/model-querying-finders.html
 // https://sequelize.org/master/manual/model-querying-basics.html#applying-where-clauses
 // https://sequelize.org/master/manual/assocs.html#-code-foo-belongsto-bar---code-
-async function addPropertyToOwner() {
-  try {
-    const property = await db.property.findOne({
-      where: {
-        name: 'Brookvista'
-      }
-    })
-
-    const owner = await db.owner.findOne({
-      where: {
-        name: 'Yuki'
-      }
-    })
-
-    owner.addProperty(property)
-
-  } catch (err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see the output
-// addPropertyToOwner()
 
 // 4. CREATE an owner named Erin age 21 who owns a property named Green Haven with 40 units 
 // BONUS add more than one proptery at the time of creation
 // https://sequelize.org/master/manual/creating-with-associations.html#hasmany---belongstomany-association
-async function createOwnerAndProperty() {
-  try {
-    const newOwner = {
-      name: 'Erin',
-      age: 21,
-      properties: [{
-          name: 'Green Haven',
-          units: 40
-        }, {
-          name: 'Fair Creek',
-          units: 35
-        }] 
-    } 
-
-    const options = {
-      include: [db.property]
-    }
-
-    const owner = await db.owner.create(newOwner, options)
-    owner.properties.forEach(property => {
-      console.log(`new owner ${owner.name} owns ${property.name}`)
-    })
-  } catch (err) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
-
-// uncomment to see output
-// createOwnerAndProperty()
 
 // 5. READ the owner named Erin and console log all of thier properties
 // https://sequelize.org/master/manual/eager-loading.html#fetching-a-single-associated-element
-async function readProperties() {
-  try {
-    const owner = await db.owner.findOne({
-      where: {
-        name: 'Erin'
-      },
-      include: [db.property]
-    })
-    owner.properties.forEach(property => {
-      console.log(property.name)
-    })
-  } catch (err){
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n", err)
-  }
-}
 
-// uncomment to see output
-// readProperties()
